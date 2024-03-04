d29adc340ab323903019a35f4873a59fd74866c1
Elvis <barnersh@gmail.com>
2024-03-04 12:17:00 +0800

    try

--- src/WalletServiceProvider.php
+++ src/WalletServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Elvis\WalletSdk;
+namespace Elvis\WalletSdk\Managers\DepositManager;
 
 use Elvis\WalletSdk\Managers\DepositManager;
 use Illuminate\Support\ServiceProvider;
@@ -10,7 +10,7 @@ class WalletServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton('wallet-sdk-deposit', function () {
-            return new DepositManager();
+            return new DepositManager;
         });
     }
 }
