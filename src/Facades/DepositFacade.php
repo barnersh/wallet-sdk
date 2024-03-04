bb59b0c4aec39a2142c49e618730776b801c44f8
Elvis <barnersh@gmail.com>
2024-03-04 11:59:09 +0800

    add comment to facade

--- src/Facades/DepositFacade.php
+++ src/Facades/DepositFacade.php
@@ -4,6 +4,9 @@ namespace Elvis\WalletSdk\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static manualConfirm()
+ */
 class DepositFacade extends Facade
 {
     protected static function getFacadeAccessor()
